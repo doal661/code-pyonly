7d1973d7ef17b5fe747453aeb2ea6e1c6923d9ed
doal61 <rlaxoan9327@gmail.com>
2025-12-10 14:56:23 +0900

    donot event process with if statement

--- event_print.py
+++ event_print.py
@@ -4,7 +4,13 @@ import sys
 input_month = 0
 input_end_of_date = 0
 
-if sys.argv[1] == str and sys.argv[2] == str:
+# print(type(sys.argv[1])) check 완료
+
+# 무조건 string으로 입력되기 때문에 if문으로 string 체크하는 의미가 없음.
+if sys.argv[1] == None or sys.argv[2] == None:
+    input_month = 0
+    input_end_of_date = 0
+else:
     input_month = int(sys.argv[1])
     input_end_of_date = int(sys.argv[2])
 
@@ -16,4 +22,5 @@ def print_to_dates_on_one_month(month, date_of_end):
 
 # print_to_dates_on_one_month(3,31)
 print_to_dates_on_one_month(input_month, input_end_of_date)
-# print(sys.argv[1])
+print(sys.argv[1])
+print(sys.argv[2])
