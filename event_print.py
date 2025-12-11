e03aa86738ed159789ae04a3648498c0432d6d1a
doal61 <rlaxoan9327@gmail.com>
2025-12-11 09:12:35 +0900

    divide comment and codes

--- event_print.py
+++ event_print.py
@@ -1,39 +1,6 @@
 # Using obsidian as checkbox in TERMINAL
 import sys
 
-# 매개변수로 돌아가는 프로그램을 매개변수 없이 돌리면 아예 sys.argv[1] 자체를 인식하지 못해서 
-# if (sys.argv[1] == None):
-#        ~~~~~~~~^^^
-#IndexError: list index out of range
-# 과 같은 에러가 뜨고 말음.
-
-
-# print(type(sys.argv[1])) check 완료
-
-# 무조건 string으로 입력되기 때문에 if문으로 string 체크하는 의미가 없음.
-#if sys.argv[1] == None or sys.argv[2] == None:
-#    input_month = 0
-#    input_end_of_date = 0
-#else:
-#    input_month = int(sys.argv[1])
-#    input_end_of_date = int(sys.argv[2])
-
-# -------------------------------
-
-# if (sys.argv[1] == None):
-#     sys.argv.append(0)
-
-# try : input_month = int(sys.argv[1]) (IndexError)
-# finally: 
-    # sys.argv[1] = 0 // list assignment index out of range
-#     sys.argv.append(0)
-# try : input_end_of_date = int(sys.argv[2]) (IndexError)
-# finally:
-#     sys.argv[2] = 0
-
-## 터미널로 쓸거면
-# if sys.argv[1] == None: # 입력이 진짜로 없으면 인터프리터는 이 부분에서 에러를 냄.
-
 # 전역변수 초기화
 input_month = -9
 input_end_of_date = -55
@@ -52,18 +19,6 @@ def argv_length_check():
     return False
 
 
-iterate_seven_days = ["sunday", "monday", "tuesday", "wendsday", "thursday", "friday", "saturday"] # 이건 7개라 하드코딩해도 문제없음.
-
-# 카운트를 위해 사용될 숫자와의 매칭을 위하여 2차원 배열을 쓴다면?
-iterate_seven_days_list = [
-    ["sun", "sunday", 0],
-    ["mon", "monday", 1],
-    ["tue", "tuesday", 2],
-    ["wen", "wendsday", 3],
-    ["thu", "thursday", 4],
-    ["fri", "friday", 5],
-    ["sat", "saturday", 6]
-]
 
 # 아니면 2개만 매칭되는걸 이용해서 2차원배열 말고 딕셔너리로 퉁친다면?
 # 라고 세번째 매개변수 고려하기 전까진 생각했었다.
@@ -106,13 +61,7 @@ def print_to_dates_on_one_month(month, date_of_end):
         date_counter = date_counter + 1
         week_counter = week_counter + 1
 
-# print_to_dates_on_one_month(3,31)
 
-
-
-# 불필요하니까 일단 주석처리
-# print(sys.argv[1])
-# print(sys.argv[2])
 
 if __name__ == "__main__":
     can_run_it = argv_length_check()
