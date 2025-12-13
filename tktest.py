78071f5230440a01a46fb69b043d5199dc28b86c
doal61 <rlaxoan9327@gmail.com>
2025-12-13 15:46:10 +0900

    add readme

--- tktest.py
+++ tktest.py
@@ -1,1 +1,5 @@
-import tk
+import tkinter as tk # 또는 from tkinter import *
+window = tk.Tk() # Tk 객체 생성 (메인 윈도우)
+window.title("나의 첫 GUI 창") # 창 제목 설정
+window.geometry("400x300") # 창 크기 설정 (가로x세로)
+window.mainloop() # 이벤트 루프 시작 (창이 닫힐 때까지 유지)
